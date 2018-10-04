c9eb7876969a0bf39628eb4b6b4f992bcacef3e2
zaksh <zohaib.sharani@gmail.com>
2018-10-04 10:18:08 +0500

    Removed unused using statements
    Resolves https://github.com/Ashera138/ADHDmail/issues/2

--- src/ADHDmail/Email/Email.cs
+++ src/ADHDmail/Email/Email.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ADHDmail.Email
+﻿namespace ADHDmail.Email
 {
     /// <summary>
     /// Represents a standard email.
