66d84d3af70e4f71213efaeacb903549c294e2e1
Colton Bouchard <coltonbouchard33@gmail.com>
2018-11-12 19:42:35 -0600

    Remove unused using statements.

--- src/ADHDmail/Extensions.cs
+++ src/ADHDmail/Extensions.cs
@@ -1,10 +1,5 @@
-﻿using ADHDmail.Config;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ADHDmail
 {
