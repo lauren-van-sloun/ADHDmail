de2de64787175b9a45bed19482c0cf5679fb7ab4
Lauren Van Sloun <vanslounl@yahoo.com>
2018-12-04 15:10:06 -0500

    Added tests for the GmailQuery class

---
+++ src/ADHDmailTests/ADHDmailUnitTests/API/GmailQueryTests.cs
@@ -0,0 +1,53 @@
+﻿using ADHDmail.API;
+using ADHDmail.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ADHDmailTests.API
+{
+    public class GmailQueryTests
+    {
+        public static IEnumerable<object[]> GmailQueriesAndTheirStringValues =>
+           new List<object[]>
+           {
+                new object[] 
+                {
+                    new GmailQuery(),
+                    string.Empty
+                },
+                new object[] 
+                {
+                    new GmailQuery(new List<Filter>()),
+                    string.Empty
+                },
+                new object[] 
+                {
+                    new GmailQuery(new List<Filter>()
+                    {
+                        new Filter(FilterOption.ContainsWord, "Test")
+                    }),
+                    "Test"
+                },
+                new object[] 
+                {
+                    new GmailQuery(new List<Filter>()
+                    {
+                        new Filter(FilterOption.HasAttachment),
+                        new Filter(FilterOption.LargerThan, "1")
+                    }),
+                    "has:attachment larger:1"
+                }
+           };
+
+        [Theory]
+        [MemberData(nameof(GmailQueriesAndTheirStringValues))]
+        public void ConstructorTest(GmailQuery gmailQuery, string expectedQueryString)
+        {
+            Assert.Equal(gmailQuery.ToString(), expectedQueryString);
+        }
+    }
+}
